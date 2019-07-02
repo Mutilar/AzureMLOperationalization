4eb988c52084c0781c142f124cfb3eb40254b739
Brian Hungerman <brianhungerman@gmail.com>
2019-07-02 15:00:21 -0700

    Fixing relative paths to test files

--- run_notebook/test_handlers.py
+++ run_notebook/test_handlers.py
@@ -5,22 +5,22 @@ from . import file_handler as fh
 
 def test_parse_and_validate_parameters():
 
-    params_input = f'./tests/params/hello-world.json'
+    params_input = f'./run_notebook/tests/params/hello-world.json'
 
     assert fh.parse_and_validate_parameters(fh.get_file_str(params_input)) is not None
 
 
 def test_add_service_bus_dependency():
 
-    conda_input = f'./tests/conda-files/inputs/hello-world.yml'
-    conda_output = f'./tests/conda-files/outputs/hello-world.yml'
+    conda_input = f'./run_notebook/tests/conda-files/inputs/hello-world.yml'
+    conda_output = f'./run_notebook/tests/conda-files/outputs/hello-world.yml'
 
     assert fh.add_service_bus_dependency(fh.get_file_str(conda_input)) == fh.get_file_str(conda_output)
 
 
 def test_add_notebook_callback():
 
-    notebook_input = f'./tests/notebooks/inputs/hello-world.ipynb'
-    notebook_output = f'./tests/notebooks/outputs/hello-world.ipynb'
+    notebook_input = f'./run_notebook/tests/notebooks/inputs/hello-world.ipynb'
+    notebook_output = f'./run_notebook/tests/notebooks/outputs/hello-world.ipynb'
 
     assert fh.add_notebook_callback(fh.get_file_str(notebook_input)) == fh.get_file_str(notebook_output)
