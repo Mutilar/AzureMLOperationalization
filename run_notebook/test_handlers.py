7ffb38659ce76c075b5be12653301b55bad91068
Brian Hungerman <brianhungerman@gmail.com>
2019-07-02 15:34:41 -0700

    Testing logic functions instead of wrapper functions (still 100% coverage)

--- run_notebook/test_handlers.py
+++ run_notebook/test_handlers.py
@@ -1,26 +1,27 @@
-from . import file_handler as fh
-
 # File Handler Unit Tests
 # -----------------------
 
+from . import file_handler as fh
+
+
 def test_parse_and_validate_parameters():
 
-    params_input = f'./run_notebook/tests/params/hello-world.json'
+    params_input = "./run_notebook/tests/params/hello-world.json"
 
     assert fh.parse_and_validate_parameters(fh.get_file_str(params_input)) is not None
 
 
-def test_add_service_bus_dependency():
+def test_inject_pip_dependency():
 
-    conda_input = f'./run_notebook/tests/conda-files/inputs/hello-world.yml'
-    conda_output = f'./run_notebook/tests/conda-files/outputs/hello-world.yml'
+    conda_input = "./run_notebook/tests/conda-files/inputs/hello-world.yml"
+    conda_output = "./run_notebook/tests/conda-files/outputs/hello-world.yml"
 
-    assert fh.add_service_bus_dependency(fh.get_file_str(conda_input)) == fh.get_file_str(conda_output)
+    assert fh.inject_pip_dependency(fh.get_file_str(conda_input), "test-pip-dependency") == fh.get_file_str(conda_output)
 
 
-def test_add_notebook_callback():
+def test_inject_notebook_try_catches():
 
-    notebook_input = f'./run_notebook/tests/notebooks/inputs/hello-world.ipynb'
-    notebook_output = f'./run_notebook/tests/notebooks/outputs/hello-world.ipynb'
+    notebook_input = "./run_notebook/tests/notebooks/inputs/hello-world.ipynb"
+    notebook_output = "./run_notebook/tests/notebooks/outputs/hello-world.ipynb"
 
-    assert fh.add_notebook_callback(fh.get_file_str(notebook_input)) == fh.get_file_str(notebook_output)
+    assert fh.inject_notebook_try_catches(fh.get_file_str(notebook_input)) == fh.get_file_str(notebook_output)
