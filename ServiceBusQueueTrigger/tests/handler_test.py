91782e67c47512b16b2cf84bc542afcb4a4b5741
Brian Hungerman <brianhungerman@gmail.com>
2019-07-02 14:26:43 -0700

    Adding :'s to function headers....

--- ServiceBusQueueTrigger/tests/handler_test.py
+++ ServiceBusQueueTrigger/tests/handler_test.py
@@ -5,14 +5,14 @@
 # -----------------------
 from .. import file_handler as fh
 
-def test_parse_and_validate_parameters()
+def test_parse_and_validate_parameters():
 
     params_input = f'./params/hello-world.json'
 
     assert fh.parse_and_validate_parameters(fh.get_file_str(params_input)) is not None
 
 
-def test_add_service_bus_dependency()
+def test_add_service_bus_dependency():
 
     conda_input = f'./conda-files/inputs/hello-world.yml'
     conda_output = f'./conda-files/outputs/hello-world.yml'
@@ -20,7 +20,7 @@ def test_add_service_bus_dependency()
     assert fh.add_service_bus_dependency(fh.get_file_str(conda_input)) == fh.get_file_str(conda_output)
 
 
-def test_add_notebook_callback()
+def test_add_notebook_callback():
 
     notebook_input = f'./notebooks/inputs/hello-world.ipynb'
     notebook_output = f'./notebooks/outputs/hello-world.ipynb'
