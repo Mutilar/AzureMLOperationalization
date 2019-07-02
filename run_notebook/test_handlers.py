a081c32dcca003f63885cc17e6b30d35d413c0d6
Brian Hungerman <brianhungerman@gmail.com>
2019-07-02 14:50:02 -0700

    Keeping all imports relative

--- run_notebook/test_handlers.py
+++ run_notebook/test_handlers.py
@@ -1,26 +1,26 @@
-from ..run_notebook import file_handler as fh
+from . import file_handler as fh
 
 # File Handler Unit Tests
 # -----------------------
 
 def test_parse_and_validate_parameters():
 
-    params_input = f'./params/hello-world.json'
+    params_input = f'./tests/params/hello-world.json'
 
     assert fh.parse_and_validate_parameters(fh.get_file_str(params_input)) is not None
 
 
 def test_add_service_bus_dependency():
 
-    conda_input = f'./conda-files/inputs/hello-world.yml'
-    conda_output = f'./conda-files/outputs/hello-world.yml'
+    conda_input = f'./tests/conda-files/inputs/hello-world.yml'
+    conda_output = f'./tests/conda-files/outputs/hello-world.yml'
 
     assert fh.add_service_bus_dependency(fh.get_file_str(conda_input)) == fh.get_file_str(conda_output)
 
 
 def test_add_notebook_callback():
 
-    notebook_input = f'./notebooks/inputs/hello-world.ipynb'
-    notebook_output = f'./notebooks/outputs/hello-world.ipynb'
+    notebook_input = f'./tests/notebooks/inputs/hello-world.ipynb'
+    notebook_output = f'./tests/notebooks/outputs/hello-world.ipynb'
 
     assert fh.add_notebook_callback(fh.get_file_str(notebook_input)) == fh.get_file_str(notebook_output)
