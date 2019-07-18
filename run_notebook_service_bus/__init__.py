c63dfa83c3e1b713ea46dedec196eb72c56cadd4
Brian Hungerman <brianhungerman@gmail.com>
2019-07-18 14:28:03 -0700

    Testing stringified message body in extension

--- run_notebook_service_bus/__init__.py
+++ run_notebook_service_bus/__init__.py
@@ -26,11 +26,11 @@ def main(msg: func.ServiceBusMessage):
 
 
     # Converts bytes into JSON
-    params = msg.get_body() #= load(
-    #    msg.get_body().decode("utf-8")
-    # )
+    params = load(
+       msg.get_body().decode("utf-8")
+    )
 
-    raise Exception(str(msg.get_body()) + "\n" + load(str(msg.get_body())))
+    raise Exception(str(msg.get_body()) + "\n" + str(msg.get_body().decode("utf-8")) + "\n" + load( msg.get_body().decode("utf-8") ))
 
     # Kicks off test runs to Azure ML Compute, called from a CI pipeline
     if params["job"] == START_BUILD:
