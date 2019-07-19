0968a3425d81645ce7bbf4aac846256043dbe900
Brian Hungerman <brianhungerman@gmail.com>
2019-07-18 19:19:31 -0700

    Removing extra dependencies

--- run_notebook_service_bus/__init__.py
+++ run_notebook_service_bus/__init__.py
@@ -1,31 +1,8 @@
 import azure.functions as func
 from yaml import safe_load as load
-from base64 import b64encode as encode
-from time import sleep
-import sys
-sys.path.append("handlers")
-import file_handler as fh
-import azureml_handler as ah
-import devops_handler as dh
 
-# Job types
-START_BUILD = "!START"
-UPDATE_BUILD = "!UPDATE"
-
-# States of pipelines
-PASSED_PIPELINE = "Succeeded"
-FAILED_PIPELINE = "Failed"
-
-# Run Conditions for pipelines
-ALL_NOTEBOOKS_MUST_PASS = "all_pass"
-
-# Directories
-OUTPUT_NOTEBOOK_LOCATION = "snapshot/outputs/output.ipynb"
 
 def main(msg: func.ServiceBusMessage):
-
-    # Converts bytes into JSON
-    # https://docs.microsoft.com/en-us/python/api/azure-functions/azure.functions.servicebusmessage?view=azure-python
 
     try:
         params = load(
