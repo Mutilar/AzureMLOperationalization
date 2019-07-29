652fd7d8424adc43beb53347649c0168d514cdce
Brian Hungerman <brianhungerman@gmail.com>
2019-07-29 13:59:51 -0700

    Fixing unit test for pip dependencies

--- unit_testing/test_handlers.py
+++ unit_testing/test_handlers.py
@@ -27,11 +27,11 @@ def test_get_file_str():
     ) == "hello world"
 
 
-def test_inject_pip_dependencies():
+def test_inject_pip_dependency():
 
-    assert fh.inject_pip_dependencies(
+    assert fh.inject_pip_dependency(
         fh.get_file_str(CONDA_INPUT),
-        ["test-pip-dependency"]
+        "test-pip-dependency"
     ) == fh.get_file_str(CONDA_OUTPUT)
 
 
