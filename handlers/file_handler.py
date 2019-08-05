f9fc873018d02baeeaf8764f9ad572f6e2727bb4
Brian Hungerman <brianhungerman@gmail.com>
2019-08-05 15:05:08 -0700

    Only downloading notebooks folder

--- handlers/file_handler.py
+++ handlers/file_handler.py
@@ -260,7 +260,7 @@ def fetch_repo(repo, version, auth_token):
     #   or commit hash (e.g. "bb7ad65dbc727ec09fe0613d51ce8585087de1b1")
     res = requests.get(
         # repo + "/archive/" + version + ".zip"
-        f'https://msdata.visualstudio.com/DefaultCollection/3adb301f-9ede-41f2-933b-fcd1a486ff7f/_apis/git/repositories/1f1e7f17-65c5-4d5a-a5fa-487802b4e71b/Items?path=/&versionDescriptor[versionOptions]=0&versionDescriptor[versionType]=0&versionDescriptor[version]=brhung/fix-automl-release-json-duplicates&resolveLfs=true&$format=zip&api-version=5.0-preview.1',
+        f'https://msdata.visualstudio.com/DefaultCollection/3adb301f-9ede-41f2-933b-fcd1a486ff7f/_apis/git/repositories/1f1e7f17-65c5-4d5a-a5fa-487802b4e71b/Items?path=/notebooks&versionDescriptor[versionOptions]=0&versionDescriptor[versionType]=0&versionDescriptor[version]=brhung/fix-automl-release-json-duplicates&resolveLfs=true&$format=zip&api-version=5.0-preview.1',
         headers = {
             'Authorization': f'Bearer {auth_token}'
         }
