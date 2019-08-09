b47cc03cd809fe986318db8df0eb632cb984ecdf
Brian Hungerman <brianhungerman@gmail.com>
2019-08-09 13:40:11 -0700

    reworking magic function logic work-around

--- handlers/notebook_handler.py
+++ handlers/notebook_handler.py
@@ -145,16 +145,7 @@ class Notebook:
                 if any(magic in self.notebook_json["cells"][cell]["source"][counter] for magic in MAGIC_FUNCTIONS):
                     if magic == "%%writefile":
                         file_name = self.notebook_json["cells"][cell]["source"][counter].split(" ")[-1]
-                        file = open(
-                            file_name, 
-                            "w+"
-                        )
-                        shutil.move(
-                            file_name,
-                            folder
-                        )
-                    del self.notebook_json["cells"][cell]["source"][counter]
-                    cell_size -= 1
+                        self.notebook_json["cells"][cell]["source"][counter] = "_FILE=open(\"" + file_name + "\", \"w+\")"
                 else:
                     counter += 1
 
